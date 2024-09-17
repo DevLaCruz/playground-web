4fa77a392a2f9ac6bf6379c0290bc467f8de76b2
Alejandro De La Cruz <97196517+DevLaCruz@users.noreply.github.com>
2024-09-17 00:00:45 -0500

    update-08-17

--- playground/registration/models.py
+++ playground/registration/models.py
@@ -4,16 +4,22 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 # Create your models here.
 
-def custom_upload_to(instance,filename):
+
+def custom_upload_to(instance, filename):
     old_instance = Profile.objects.get(id=instance.pk)
     old_instance.avatar.delete()
     return 'profiles/' + str(instance.pk) + '_' + filename
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.ImageField(upload_to=custom_upload_to, null=True, blank=True)
+    avatar = models.ImageField(
+        upload_to=custom_upload_to, null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
     link = models.URLField(max_length=200, null=True, blank=True)
+
+    class Meta:
+        ordering = ['user__username']
 
 
 @receiver(post_save, sender=User)
